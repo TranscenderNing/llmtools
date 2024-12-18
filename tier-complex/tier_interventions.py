47fa5fee4541f551936de2d29061bdeca3700b41
ning <1907964014@qq.com>
2024-12-18 11:26:37 +0800

    delete interventions

--- tier-complex/tier_interventions.py
+++ tier-complex/tier_interventions.py
@@ -132,39 +132,6 @@ class LoreftIntervention_v2(nn.Module):
         )
         return self.dropout(output.to(base.dtype))
 
-    def state_dict(self, *args, **kwargs):
-        """
-        Overwrite for data-efficiency.
-        """
-        state_dict = OrderedDict()
-        for k, v in self.learned_source.state_dict().items():
-            state_dict[k] = v
-        state_dict["rotate_layer"] = self.rotate_layer.weight.data
-        return state_dict
-
-    def load_state_dict(self, state_dict, *args, **kwargs):
-        """
-        Overwrite for data-efficiency.
-        """
-        self.learned_source.load_state_dict(state_dict, strict=False)
-
-        # Caveat: without creating a new layer, it might not work (still not sure why)
-        # We have to recreate a layer, and load back the columns.
-        overload_w = state_dict["rotate_layer"].to(self.learned_source.weight.device)
-        overload_w_width = overload_w.shape[-1]
-        rotate_layer = LowRankRotateLayer(
-            self.embed_dim, overload_w_width, init_orth=True
-        ).to(self.learned_source.weight.device)
-        self.rotate_layer = torch.nn.utils.parametrizations.orthogonal(rotate_layer)
-        self.rotate_layer.parametrizations.weight[0].base[
-            :, :overload_w_width
-        ] = overload_w
-        assert (
-            torch.allclose(self.rotate_layer.weight.data, overload_w.data) == True
-        )  # we must match!
-
-        return
-
 
 class MloraIntervention(nn.Module):
     """
@@ -213,26 +180,6 @@ class MloraIntervention(nn.Module):
         output = x + out_x
         return self.dropout(output.to(x.dtype))
 
-    def state_dict(self, *args, **kwargs):
-        """
-        Overwrite for data-efficiency.
-        """
-        state_dict = OrderedDict()
-        for k, v in self.learned_source.state_dict().items():
-            state_dict[k] = v
-        return state_dict
-
-    def load_state_dict(self, state_dict, *args, **kwargs):
-        """
-        Overwrite for data-efficiency.
-        """
-        self.learned_source.load_state_dict(state_dict, strict=False)
-
-        # Caveat: without creating a new layer, it might not work (still not sure why)
-        # We have to recreate a layer, and load back the columns.
-
-        return
-
 
 class MosLoRAIntervention(nn.Module):
     """
@@ -277,39 +224,6 @@ class MosLoRAIntervention(nn.Module):
             self.rotate_layer.weight.T,
         )
         return self.dropout(output.to(base.dtype))
-
-    def state_dict(self, *args, **kwargs):
-        """
-        Overwrite for data-efficiency.
-        """
-        state_dict = OrderedDict()
-        for k, v in self.learned_source.state_dict().items():
-            state_dict[k] = v
-        state_dict["rotate_layer"] = self.rotate_layer.weight.data
-        return state_dict
-
-    def load_state_dict(self, state_dict, *args, **kwargs):
-        """
-        Overwrite for data-efficiency.
-        """
-        self.learned_source.load_state_dict(state_dict, strict=False)
-
-        # Caveat: without creating a new layer, it might not work (still not sure why)
-        # We have to recreate a layer, and load back the columns.
-        overload_w = state_dict["rotate_layer"].to(self.learned_source.weight.device)
-        overload_w_width = overload_w.shape[-1]
-        rotate_layer = LowRankRotateLayer(
-            self.embed_dim, overload_w_width, init_orth=True
-        ).to(self.learned_source.weight.device)
-        self.rotate_layer = torch.nn.utils.parametrizations.orthogonal(rotate_layer)
-        self.rotate_layer.parametrizations.weight[0].base[
-            :, :overload_w_width
-        ] = overload_w
-        assert (
-            torch.allclose(self.rotate_layer.weight.data, overload_w.data) == True
-        )  # we must match!
-
-        return
 
 
 class RedIntervention(nn.Module):
@@ -440,25 +354,6 @@ class LoRAIntervention(nn.Module):
             * self.scaling
         )
         return result.to(base.dtype)
-
-    def state_dict(self, *args, **kwargs) -> OrderedDict:
-        """Overwrite for data-efficiency."""
-        state_dict = OrderedDict()
-        # state_dict.update(self.lora_A.state_dict())
-        # state_dict.update(self.lora_B.state_dict())
-
-        state_dict.update(
-            {
-                "lora_A": self.lora_A.data,
-                "lora_B": self.lora_B.data,
-            }
-        )
-        return state_dict
-
-    def load_state_dict(self, state_dict: dict, *args, **kwargs) -> None:
-        """Overwrite for data-efficiency."""
-        self.lora_A.load_state_dict(state_dict, strict=False)
-        self.lora_B.load_state_dict(state_dict, strict=False)
 
 
 class FeedForwardIntervention(nn.Module):
@@ -591,145 +486,6 @@ class MultiHeadAttentionIntervention(nn.Module):
             self.bias,
             self.activation,
         )
-
-
-class NoreftIntervention(nn.Module):
-    """
-    NoReFT(h) = h + W2^T(W1h + b − W2h)
-    """
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs, keep_last_dim=True)
-        self.proj_layer = torch.nn.Linear(
-            self.embed_dim, kwargs["low_rank_dimension"], bias=kwargs["add_bias"]
-        ).to(kwargs["dtype"] if "dtype" in kwargs else torch.bfloat16)
-        self.learned_source = torch.nn.Linear(
-            self.embed_dim, kwargs["low_rank_dimension"]
-        ).to(kwargs["dtype"] if "dtype" in kwargs else torch.bfloat16)
-        self.dropout = torch.nn.Dropout(
-            kwargs["dropout"] if "dropout" in kwargs else 0.0
-        )
-        self.act_fn = (
-            ACT2FN["linear"]
-            if "act_fn" not in kwargs or kwargs["act_fn"] is None
-            else ACT2FN[kwargs["act_fn"]]
-        )
-
-    def forward(self, base):
-        proj_base = self.proj_layer(base)
-        output = base + torch.matmul(
-            (self.act_fn(self.learned_source(base)) - proj_base), self.proj_layer.weight
-        )
-        return self.dropout(output.to(base.dtype))
-
-
-class ConsreftIntervention(nn.Module):
-    """
-    ConsReFT(h) = h + R^T(b − Rh)
-    """
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs, keep_last_dim=True)
-        rotate_layer = LowRankRotateLayer(
-            self.embed_dim, kwargs["low_rank_dimension"], init_orth=True
-        )
-        self.rotate_layer = torch.nn.utils.parametrizations.orthogonal(rotate_layer)
-        self.learned_source = torch.nn.Parameter(
-            torch.rand(kwargs["low_rank_dimension"]), requires_grad=True
-        )
-
-    def forward(self, base):
-        rotated_base = self.rotate_layer(base)
-        output = base + torch.matmul(
-            (self.learned_source - rotated_base), self.rotate_layer.weight.T
-        )
-        return output.to(base.dtype)
-
-
-class LobireftIntervention(nn.Module):
-    """
-    LobiReFT(h) = h + R^T(b)
-    """
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs, keep_last_dim=True)
-        rotate_layer = LowRankRotateLayer(
-            self.embed_dim, kwargs["low_rank_dimension"], init_orth=True
-        )
-        self.rotate_layer = torch.nn.utils.parametrizations.orthogonal(rotate_layer)
-        self.learned_source = torch.nn.Parameter(
-            torch.rand(kwargs["low_rank_dimension"]), requires_grad=True
-        )
-        self.dropout = torch.nn.Dropout(
-            kwargs["dropout"] if "dropout" in kwargs else 0.0
-        )
-
-    def forward(self, base):
-        output = base + torch.matmul(self.learned_source, self.rotate_layer.weight.T)
-        return self.dropout(output.to(base.dtype))
-
-
-class DireftIntervention(nn.Module):
-    """
-    DiReFT(h) = h + R^T(Wh + b)
-    """
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs, keep_last_dim=True)
-        rotate_layer = LowRankRotateLayer(
-            self.embed_dim, kwargs["low_rank_dimension"], init_orth=True
-        )
-        self.rotate_layer = torch.nn.utils.parametrizations.orthogonal(rotate_layer)
-        self.learned_source = torch.nn.Linear(
-            self.embed_dim, kwargs["low_rank_dimension"]
-        ).to(kwargs["dtype"] if "dtype" in kwargs else torch.bfloat16)
-        self.dropout = torch.nn.Dropout(
-            kwargs["dropout"] if "dropout" in kwargs else 0.0
-        )
-        self.act_fn = (
-            ACT2FN["linear"]
-            if "act_fn" not in kwargs or kwargs["act_fn"] is None
-            else ACT2FN[kwargs["act_fn"]]
-        )
-
-    def forward(self, base):
-        cast_base = base.to(self.learned_source.weight.dtype)
-        output = base + torch.matmul(
-            (self.act_fn(self.learned_source(cast_base))).to(
-                self.rotate_layer.weight.dtype
-            ),
-            self.rotate_layer.weight.T,
-        )
-        return self.dropout(output.to(base.dtype))
-
-
-class NodireftIntervention(nn.Module):
-    """
-    NodiReFT(h) = h + W2^T(W1h + b)
-    """
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs, keep_last_dim=True)
-        self.proj_layer = torch.nn.Linear(
-            self.embed_dim, kwargs["low_rank_dimension"], bias=kwargs["add_bias"]
-        ).to(kwargs["dtype"] if "dtype" in kwargs else torch.bfloat16)
-        self.learned_source = torch.nn.Linear(
-            self.embed_dim, kwargs["low_rank_dimension"]
-        ).to(kwargs["dtype"] if "dtype" in kwargs else torch.bfloat16)
-        self.dropout = torch.nn.Dropout(
-            kwargs["dropout"] if "dropout" in kwargs else 0.0
-        )
-        self.act_fn = (
-            ACT2FN["linear"]
-            if "act_fn" not in kwargs or kwargs["act_fn"] is None
-            else ACT2FN[kwargs["act_fn"]]
-        )
-
-    def forward(self, base):
-        output = base + torch.matmul(
-            self.act_fn(self.learned_source(base)), self.proj_layer.weight
-        )
-        return self.dropout(output.to(base.dtype))
 
 
 ############################## moe related ################################
